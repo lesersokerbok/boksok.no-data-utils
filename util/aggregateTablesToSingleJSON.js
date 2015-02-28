2efd6c817d6ceb4cfd65c90f7fd6d13c7cc266b8
Tom Erik Støwer <testower@gmail.com>
2015-02-28 22:20:04 +0100

    Fix json aggregation methods

--- util/aggregateTablesToSingleJSON.js
+++ util/aggregateTablesToSingleJSON.js
@@ -2,20 +2,20 @@
 
 var fs = require('fs'),
     tablesToJSON = require('../lib/tables-to-json.js'),
-    textCategories = JSON.parse(fs.readFileSync('./original-data/JSON-FROM-CSV/TEXT.json')),
-    menuLinks = JSON.parse(fs.readFileSync('./original-data/JSON-FROM-CSV/MENU_LINK.json')),
-    menuItems = JSON.parse(fs.readFileSync('./original-data/JSON-FROM-CSV/MENU.json')),
-    contentItems = JSON.parse(fs.readFileSync('./original-data/JSON-FROM-CSV/CONTENT.json'));
+    textCategories = JSON.parse(fs.readFileSync('./input/01-15/JSON-FROM-CSV/TEXT.json')),
+    menuLinks = JSON.parse(fs.readFileSync('./input/01-15/JSON-FROM-CSV/MENU_LINK.json')),
+    menuItems = JSON.parse(fs.readFileSync('./input/01-15/JSON-FROM-CSV/MENU.json')),
+    contentItems = JSON.parse(fs.readFileSync('./input/01-15/JSON-FROM-CSV/CONTENT.json'));
 
 
 
-fs.readFile('./original-data/JSON-FROM-CSV/DOC.json', function (err, data) {
+fs.readFile('./input/01-15/JSON-FROM-CSV/DOC.json', function (err, data) {
 
     var list = JSON.parse(data);
 
     var books = tablesToJSON.convertToJSON(list, textCategories, menuLinks, menuItems, contentItems);
 
-    fs.writeFile('./output-example/test-json-conversion.json', JSON.stringify(books, null, 4), function (err) {
+    fs.writeFile('./output/01-15/test-json-conversion.json', JSON.stringify(books, null, 4), function (err) {
         if (err)
             console.log(err);
     });
